cc12785348bf28800a2d1a70b128e52da431ae1d
ErosFranklin <eros.felix@aluno.uepb.edu.br>
2024-04-23 18:57:45 -0300

    alteration css

--- javascript/script_eyes.js
+++ javascript/script_eyes.js
@@ -1,11 +1,26 @@
 function mostrarSenha(){
     var input = document.getElementById('password');
     var eyesbtt = document.getElementById('eyes');
-    if(input.type === 'password'){
+    
+    if(input.type === 'password' ){
         input.setAttribute('type','text');
-        eyesbtt.classList.replace('fa-eye', 'fa-eye-slash');
-    }else{
+        eyesbtt.classList.replace('bi-eye-fill', 'bi-eye-slash-fill');
+    }
+    else{
         input.setAttribute('type','password');
-        eyesbtt.classList.replace('fa-eye-slash', 'fa-eye');
+        eyesbtt.classList.replace('bi-eye-slash-fill', 'bi-eye-fill');
+    }
+}
+function mostrarConfSenha(){
+    var inputC = document.getElementById('confsenha');
+    var eyesbttC = document.getElementById('eyes2');
+
+    if(inputC.type === 'password' ){
+        inputC.setAttribute('type','text');
+        eyesbttC.classList.replace('bi-eye-fill', 'bi-eye-slash-fill');
+    }
+    else{
+        inputC.setAttribute('type','password');
+        eyesbttC.classList.replace('bi-eye-slash-fill', 'bi-eye-fill');
     }
 }
