315f70387c59301117a8572e0b8e65bbbf66c0c7
ErosFranklin <eros.felix@aluno.uepb.edu.br>
2024-07-22 14:02:00 -0300

    updtade in cadastro.js

--- frontend/javascript/script_cadastro.js
+++ frontend/javascript/script_cadastro.js
@@ -7,43 +7,58 @@ document.addEventListener("DOMContentLoaded", function() {
         const nome = document.querySelector("#nome").value;
         const email = document.querySelector("#email").value;
         const dataNasc = document.querySelector("#dataNasc").value;
-        const senha = document.querySelector("#senha").value;
+        const password = document.querySelector("#password").value;
         const confsenha = document.querySelector("#confsenha").value;
         
-        if (!validarEmail(email)) {
-            alert("E-mail inválido.");
+        if (nome.value === "" || email.value === "" || dataNasc === "" || password === "" || confsenha === "") {
+            alert("Preencha todos os campos!");
             return;
+        } else if(
+            validarEmail(email.value) &&
+            validarPassword(password.value) &&
+            validarSenhas(password.value, confsenha.value)
+        ){
+            const usuario = {
+                nome:nome.value,
+                email:email.value,
+                dataNasc:dataNasc.value,
+                password:password.value,
+                confsenha:confsenha.value
+            }
+            // Enviar os dados para o servidor
+        try {
+            const response = await fetch("http://localhost:8000/cadastro", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(usuario)
+        });
+  
+        if (!response.ok) {
+          throw new Error("Erro na requisição");
         }
-
-        if (senha !== confsenha) {
-            alert("As senhas digitadas não coincidem.");
-            return;
-        }
-
-        enviar() 
-        document.querySelector("#nome").value = "";
-        document.querySelector("#email").value = "";
-        document.querySelector("#dataNasc").value = "";
-        document.querySelector("#senha").value = "";
-        document.querySelector("#confsenha").value = "";
-
-    });
+  
+        // Redirecionar para a tela de login
+        window.location.href = "/views/login.html";
+      } catch (error) {
+        console.error('Erro:', error);
+        textForm.textContent = "Erro ao cadastrar usuário!";
+      }
+    } else {
+      alert("Verifique os campos e tente novamente!") 
+    }
 });
     
 function validarEmail(email) {
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var emailRegex = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
     return emailRegex.test(email);
 }
-
-function enviar() {
-    alert("Cadastro realizado.");   
+function validarPassword(password){
+    var passwordRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]{6,16}$/;\
+    return passwordRegex.test(password);
 }
-
-
-
-
-
-
-
-
-        
+function validarSenhas(password, confsenha){
+    return password === confsenha
+}
+})
