664a28e43f798f14d45330510bef72c29e1f0611
ErosFranklin <eros.felix@aluno.uepb.edu.br>
2024-08-06 15:47:36 -0300

    update pos-autenticacao-aluno.html

--- frontend/javascript/pos-autenticacao-aluno.js
+++ frontend/javascript/pos-autenticacao-aluno.js
@@ -1,92 +1,91 @@
 document.addEventListener('DOMContentLoaded', function(){
     const name = localStorage.getItem('nome');
     const dataNasc = localStorage.getItem('dataNasc');
-    console.log(dataNasc)
 
     if (name) {
         const nameField = document.querySelector('#nome');
         nameField.value = name;
     }
     if (dataNasc) {
-       const dataNascConverted = convertDateFormat(dataNasc); 
-       const dataNascField = document.querySelector('#nascimento');
-       dataNascField.value = dataNascConverted;
-   }
-   document.querySelector('#formPosAutentProfessor').addEventListener('submit', async function(event){
-       event.preventDefault();
+        const dataNascConverted = convertDateFormat(dataNasc); 
+        const dataNascField = document.querySelector('#nascimento');
+        dataNascField.value = dataNascConverted;
+    }
 
-       const gender = document.getElementById('genero').value;
-       const period = document.getElementById('periodo').value;
-       const registration = document.getElementById('matricula').value;
-       const password = document.getElementById('password').value;
-       const city = document.getElementById('cidade').value;
-       const state = document.getElementById('estado').value;
-       const institution = document.getElementById('instituicao').value;
-       const passwordNew = dicument.getElementById('passwordPA').value;
+    const form = document.querySelector('#formPosAutentAluno')
+    form.addEventListener('submit', async function(event){
+        event.preventDefault();
 
-       if(name === "" || gender ==="" || period==="" || registration === "" || password === "" || city === "" ||
+        const name = document.getElementById('nome').value;
+        const gender = document.getElementById('genero').value;
+        const period = document.getElementById('periodo').value;
+        const registration = document.getElementById('matricula').value;
+        const city = document.getElementById('cidade').value;
+        const state = document.getElementById('estado').value;
+        const institution = document.getElementById('instituicao').value;
+        
+
+        if(name === "" || gender ==="" || period ==="" || registration === "" || password === "" || city === "" ||
         state === "" || institution ==="" || passwordNew === ""){
-           alert("Preencha todos os campos!!!");
-           return
-       }
-       if (!validarPassword(passwordNew)) {
-           alert("A senha deve conter entre 6 e 20 caracteres, pelo menos um número e uma letra.");
-           return;
-       }
-       try{
-           const url = `https://projetodepesquisa.vercel.app/api/pos-autenticacao`;
-           const data = {
-               name:name,
-               password:password,
-               gender:gender,
-               period:period,
-               registration:registration,
-               city:city,
-               state:state,
-               institution:institution,
-               passwordNew:passwordNew
-   
-           };
-           const response = await fetch(url, {
-               method: 'POST',
-               headers: {
-                   'Content-Type': 'application/json'
-               },
-               body: JSON.stringify(data)
-           });
-           if (!response.ok) {
-               const errorData = await response.json();
-               console.error('Error:', errorData.message);
-               alert('Erro: ' + errorData.message);
-               return;
-           }
-           try {
-               const responseData = await response.json();
-               alert('Cadastro realizado com sucesso!');
-               window.location.href = "../html/login.html"
-           } catch (error) {
-               console.error('JSON parse error:', error);
-               alert('Ocorreu um erro ao processar a resposta do servidor.');
-           }
+            alert("Preencha todos os campos!!!");
+            return;
+        }
 
-       } catch (error) {
-           console.error('Fetch error:', error);
-           alert('Ocorreu um erro ao tentar cadastrar. Por favor, tente novamente.');
-       }
-   })
-   function validarPassword(password){
-       let passwordRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]{6,20}$/;
-       return passwordRegex.test(password);
-   }
-})
+
+        try {
+            const url = `https://projetodepesquisa.vercel.app/api/update`;
+            const data = {
+                name: name,
+                gender: gender,
+                period: period,
+                registration: registration,
+                city: city,
+                state: state,
+                institution: institution,
+            };
+            const response = await fetch(url, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                console.error('Error:', errorData.message);
+                alert('Erro: ' + errorData.message);
+                return;
+            }
+
+            try {
+                const responseData = await response.json();
+                alert('Cadastro realizado com sucesso!');
+                window.location.href = "../html/login.html";
+            } catch (error) {
+                console.error('JSON parse error:', error);
+                alert('Ocorreu um erro ao processar a resposta do servidor.');
+            }
+        } catch (error) {
+            console.error('Fetch error:', error);
+            alert('Ocorreu um erro ao tentar cadastrar. Por favor, tente novamente.');
+        }
+    });
+
+    function validarPassword(password){
+        let passwordRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]{6,20}$/;
+        return passwordRegex.test(password);
+    }
+});
+
 function convertDateFormat(dateStr) {
-   const datePattern = /^\d{4}-\d{2}-\d{2}$/;
-   if (!datePattern.test(dateStr)) {
-       console.error("Formato de data inválido.");
-       return null;
-   }
+    const datePattern = /^\d{4}-\d{2}-\d{2}$/;
+    if (!datePattern.test(dateStr)) {
+        console.error("Formato de data inválido.");
+        return null;
+    }
 
-   const [year, month, day] = dateStr.split('-');
+    const [year, month, day] = dateStr.split('-');
 
-   return `${day}/${month}/${year}`;
+    return `${day}/${month}/${year}`;
 }
