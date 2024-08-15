b77a2a95b5ec61789cb449d0cd460c27e805edce
ErosFranklin <eros.felix@aluno.uepb.edu.br>
2024-08-15 19:56:06 -0300

    photos

--- frontend/javascript/alterar-foto-aluno.js
+++ frontend/javascript/alterar-foto-aluno.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const studentId = localStorage.getItem('userId')
+    const studentToken = localStorage.getItem('token')
     const selecionarFotoInput = document.getElementById('mudarFotoA');
     const fotoContainer = document.getElementById('fotoContainer');
     const foto = document.getElementById('fotoAluno');
 
-    selecionarFotoInput.addEventListener('change', function(event) {
+    selecionarFotoInput.addEventListener('change',async function(event) {
         const arquivo = event.target.files[0];
         if (arquivo) {
 
@@ -12,6 +14,25 @@ document.addEventListener('DOMContentLoaded', function() {
             foto.src = url;
 
             fotoContainer.style.display = 'block';
+
+            const formData = new FormData();
+            formData.append('image', arquivo)
+
+            try{
+                const response = await fetch('https://projetodepesquisa.vercel.app/api/student/upload_image',{
+                    method:'PATCH',
+                    headers:{
+                        'Authorization':  `Bearer ${studentToken}`
+                    },
+                    body:formData
+                })
+                if(!response.ok){
+                    throw new Error('Erro ao fazer o upload da imagem')
+                }
+
+            }catch(error){
+                console.log('Erro ao enviar a imagem:', error)
+            }
         }
         
     });
--- frontend/javascript/alterar-foto-professor.js
+++ frontend/javascript/alterar-foto-professor.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const teacherId = localStorage.getItem('userId');
+    const teacherToken = localStorage.getItem('token')
     const selecionarFotoInput = document.getElementById('mudarFotoP');
     const fotoContainer = document.getElementById('fotoContainer');
     const foto = document.getElementById('fotoProfessor');
 
-    selecionarFotoInput.addEventListener('change', function(event) {
+    selecionarFotoInput.addEventListener('change', async function(event) {
         const arquivo = event.target.files[0];
         if (arquivo) {
 
@@ -12,6 +14,28 @@ document.addEventListener('DOMContentLoaded', function() {
             foto.src = url;
 
             fotoContainer.style.display = 'block';
+
+            //Enviando a imagem para o servidor;
+
+            const formData = new FormData();
+            formData.append('image', arquivo)
+            try{
+                const response = await fetch('https://projetodepesquisa.vercel.app/api/teacher/upload_image',{
+                    method:'PATCH',
+                    headers:{
+                        'Authorization': `Bearer ${token}`
+                    },
+                    body:formData
+                })
+                if(!response.ok){
+                    throw new Error('Errp ao fazer o upload da imagem')
+                }
+
+                const data = await response.json()
+                console.log('Imagem enviada com sucesso:', data.file_url)
+            }catch(error){
+                console.error('Erro ao enviar a imagem:', error)
+            }
         }
     });
 });
