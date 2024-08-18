002acb778ad61ca2585765fd3a90fafde7e9d622
ErosFranklin <eros.felix@aluno.uepb.edu.br>
2024-08-18 14:45:51 -0300

    add

---
+++ frontend/javascript/firebase.js
@@ -0,0 +1,19 @@
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBdZv0pfHBnDmt_b9ZTgpubhiGCi2jbvjY',
+  authDomain: 'screen-programing-352a5.firebaseapp.com',
+  projectId: 'screen-programing-352a5',
+  storageBucket: 'screen-programing-352a5.appspot.com',
+  messagingSenderId: '66540431655',
+  appId: '1:66540431655:web:587198e317fd192e74e102',
+  measurementId: 'G-B9ZBSG6016'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
