6cc9de763e99f4193c178398f6645c05610106db
Eros Franklin <eros.felix@aluno.uepb.edu.br>
2024-10-31 15:08:30 -0300

    add atividades

--- frontend/public/javascript/atividades.js
+++ frontend/public/javascript/atividades.js
@@ -10,7 +10,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const formAddAtt = document.querySelector('#formAddAtt');
     const atividadeContainer = document.querySelector('.atividades-container')
 
-    
+    carregarAtividades(groupId);
+
     // DEPOIS OLHE SE ESTA FUNCIONANDO E PUXE COM O GET DOIDAO
     const modalExibido = localStorage.getItem("modalExibido");
     if (modalExibido === "true") {
@@ -30,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
     formAddAtt.addEventListener('submit', async function(event){
         event.preventDefault();
-        const deadline = deadlineInput.value;
+        const deadline = convertDateFormat(deadlineInput.value);
         const dataDescription = getId_Content(selectDescription);
         const description = dataDescription.description;
         const id_content = dataDescription.id_content;
@@ -53,6 +54,58 @@ document.addEventListener('DOMContentLoaded', function(){
           }
 
     })
+    async function carregarAtividades(groupId) {
+      const loader = document.querySelector(".verificando");
+      loader.style.display = "block";
+      
+      if (!groupId) {
+        console.error("Erro: ID do grupo não encontrado.");
+        loader.style.display = "none";
+        return;
+      }
+  
+      try {
+        const response = await fetch(
+          `https://projetodepesquisa-w8nz.onrender.com/api/activity/all?id_group=${groupId}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json"
+            },
+          }
+        );
+  
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData.message);
+        }
+  
+        const atividadeData = await response.json();
+        console.log(atividadeData);
+        if (Array.isArray(atividadeData.activity) && atividadeData.activity.length > 0) {
+          atividadeData.activity.forEach((atividade) => {
+            const id_activity = atividade[0]
+            const description = atividade[1];
+            const deadline = convertDateFormat(atividade[2]);
+            const id_content = atividade[3]
+            const atividadeGrupo = criarAtividade(description, deadline, id_content, id_activity);
+            console.log(atividadeGrupo)
+            atividadeContainer.appendChild(atividadeGrupo);
+          });
+        } else {
+          const mensagem = document.createElement("p");
+          mensagem.textContent = "Nenhuma atividade cadastrada.";
+          atividadeContainer.appendChild(mensagem);
+          console.error(
+            'A resposta da API não contém a propriedade "atividade" ou não é um array.'
+          );
+        }
+      } catch (error) {
+        console.error("Erro ao carregar atividade:", error);
+      }finally {
+        loader.style.display = "none"; 
+      }
+    }
     async function salvarAtividadeBackend(description, deadline, id_content) {
     
         const activityData = {
@@ -85,11 +138,21 @@ document.addEventListener('DOMContentLoaded', function(){
           return null;
         }
       }
-      function criarAtividade(description, deadline, id_activity) {
+      function criarAtividade(description, deadline, id_content, id_activity) {
+        let icone = "";
         const novaAtividade = document.createElement("div");
         novaAtividade.className = "atividade";
         novaAtividade.dataset.id_activity = id_activity;
-        novaAtividade.innerHTML = `<h2><a href="questoes.html?idAtividade=${id_activity}">${description}</a></h2><p>Data de Encerramento: ${deadline}</p>`;
+        if (id_content === 1) {
+          icone = '<i class="bi bi-database"></i>';
+        } else if (id_content === 2) {
+          icone = '<i class="bi bi-arrow-repeat"></i>';
+        } else if (id_content === 3) {
+          icone = '<i class="bi bi-code-slash"></i>';
+        } else if (id_content === 4) {
+          icone = '<i class="bi bi-keyboard"></i>';
+        }
+        novaAtividade.innerHTML = `<h2><a href="questoes.html?idAtividade=${id_activity}">${description} ${icone}</a></h2><p>Data de Encerramento: ${deadline}</p>`;
     
         const editar = document.createElement("button");
         editar.innerHTML = '<i class="bi bi-pencil-square"></i>';
@@ -127,6 +190,7 @@ document.addEventListener('DOMContentLoaded', function(){
         }
         return false;
     }
+    
     function getId_Content(description){
         const selectElement = description;
         const selectedIdContent = selectElement.value; 
@@ -147,7 +211,18 @@ document.addEventListener('DOMContentLoaded', function(){
         overlay.style.display = "none";
         modal.style.display = "none";
         localStorage.setItem("modalExibido", "false");
+    }
+    function convertDateFormat(dateStr) {
+      const datePattern = /^\d{4}-\d{2}-\d{2}$/;
+      if (!datePattern.test(dateStr)) {
+        console.error("Formato de data inválido.");
+        return null;
       }
+  
+      const [year, month, day] = dateStr.split("-");
+  
+      return `${day}/${month}/${year}`;
+    }
     
 
 
