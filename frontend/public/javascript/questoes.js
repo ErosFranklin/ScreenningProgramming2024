4bd8a6b81e22a280ae56765ca20e41aac1e1c53a
Eros Franklin <eros.felix@aluno.uepb.edu.br>
2024-12-20 14:40:14 -0300

    update questoes-aluno.html

--- frontend/public/javascript/questoes.js
+++ frontend/public/javascript/questoes.js
@@ -4,60 +4,80 @@ document.addEventListener("DOMContentLoaded", async function (){
     const alternativaC = document.querySelector("#alternativaC");
     const alternativaD = document.querySelector("#alternativaD");
     const containerQuestao = document.querySelector("#containerQuestao");
-    const enunciado = document.querySelector("#enunciado");
+    const enunciado = document.querySelector(".enunciado");
     const token = localStorage.getItem("token");
     const userId = localStorage.getItem("userId");
     const urlParametros = new URLSearchParams(window.location.search);
     const id_activity = urlParametros.get("idAtividade");
     const id_content  = urlParametros.get("id_content");
     const groupId = urlParametros.get("groupId");
+    const overlay = document.querySelector(".overlay");
+    const spinner = document.querySelector(".spinner");
 
     console.log(groupId, id_content);
 
     carregarDadosAtividade();
     carregarAlternativa();
-    async function carregarDadosAtividade(){
+    async function carregarDadosAtividade() {
         const tituloAtividade = document.querySelector("#titulo-atividade");
-        try{
-            const response = await fetch(`https://screenning-programming.onrender.com//api/activity?id_content=${id_content}&id_group=${groupId}`,{
+        try {
+            overlay.classList.add("active");
+            spinner.classList.add("active");
+    
+            const response = await fetch(`https://screenning-programming.onrender.com/api/activity?id_content=${id_content}&id_group=${groupId}`, {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
                 },
-    
             });
-            if(!response.ok){
+    
+            if (!response.ok) {
                 throw new Error("Erro ao buscar dados da atividade");
             }
+    
             const atividadeData = await response.json();
-            console.log(atividadeData)
+            console.log(atividadeData);
             tituloAtividade.innerHTML = atividadeData.activity[0][1];
-            
-        }catch(error){
+        } catch (error) {
             console.error("Erro ao buscar dados da atividade", error);
             alert("Erro ao buscar dados da atividade");
+        } finally {
+            
+            overlay.classList.remove("active");
+            spinner.classList.remove("active");
+        }
+    }
+    async function carregarAlternativa(){
+
+        try{
+            overlay.classList.add("active");
+            spinner.classList.add("active");
+
+            const response = await fetch(`https://screenning-programming.onrender.com/api/question/level?id_activity=${id_activity}`,{
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`,
+    
+                }
+            });
+            if(!response.ok){
+                throw new Error("Erro ao buscar questões");
+            }
+            const responseData = await response.json(); 
+            console.log(responseData);
+            const data = responseData[0]; 
+            const questionImageUrl = data["Question Image"];
+            const questionImageElement = document.getElementById('questao-imagem');
+            questionImageElement.src = questionImageUrl;
+        }catch(error){
+            console.error("Erro ao buscar questões", error);
+            alert("Erro ao buscar questões");
+        }finally {
+            
+            overlay.classList.remove("active");
+            spinner.classList.remove("active");
         }
         
     }
-    /*
-    async function carregarAlternativa(){
-        const response = await fetch(`https://screenning-programming.onrender.com/api/question/level`,{
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`,
-
-            },
-            body: JSON.stringify({id_activity: id_activity})
-        });
-        if(!response.ok){
-            throw new Error("Erro ao buscar questões");
-        }
-        console.log(response)
-    }
-        */
-
-
-
-
 });
